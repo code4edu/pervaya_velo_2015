da5714f84dad7ac33606a168d71a80f6e3a2d89b
aleksandr <okunev.aleksandr@inbox.ru>
2015-12-19 16:09:43 +0300

    added files api

--- MyHackathon/MyHackathon/Models/ClientModels/File.cs
+++ MyHackathon/MyHackathon/Models/ClientModels/File.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Web;
 
@@ -18,5 +19,8 @@ namespace MyHackathon.Models.ClientModels
 
 		[Required]
 		public string Path { get; set; }
+
+		[NotMapped]
+		public byte[] Data { get; set; }
 	}
 }
