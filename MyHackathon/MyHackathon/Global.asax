9d84d7be94de680b2d28b5cd372c3810f15eccab
aleksandr <okunev.aleksandr@inbox.ru>
2015-12-19 09:37:01 +0300

    init

---
+++ MyHackathon/MyHackathon/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MyHackathon.MvcApplication" Language="C#" %>
---
+++ MyHackathon/MyHackathon/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MyHackathon.Startup))]
+namespace MyHackathon
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
