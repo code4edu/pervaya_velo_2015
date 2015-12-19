b0b6adbe6e8fbe69797a9e3e3da7e799cdd9f8c9
ArturNizamutdinov <nizamutdin.art@yandex.ru>
2015-12-19 11:07:28 +0300

    Added ui-router

--- MyHackathon/MyHackathon/Controllers/HomeController.cs
+++ MyHackathon/MyHackathon/Controllers/HomeController.cs
@@ -26,5 +26,16 @@ namespace MyHackathon.Controllers
 
             return View();
         }
-    }
+
+		public ActionResult Home()
+		{
+			return View();
+		}
+
+
+		public ActionResult Login()
+		{
+			return View();
+		}
+	}
 }
---
+++ MyHackathon/MyHackathon/Scripts/App/app.js
@@ -0,0 +1,49 @@
+﻿/// <reference path="../angular.js" />
+/// <reference path="../angular/angular-ui-router.js" />
+var App = angular
+    .module('App', ['ui.router', 'ngAnimate']);
+
+App
+    .config(function ($stateProvider, $urlRouterProvider) {
+    	//
+    	// For any unmatched url, redirect to /state1
+    	$urlRouterProvider.otherwise("/");
+    	//
+    	// Now set up the states
+    	$stateProvider
+          .state('home', {
+          	url: "/",
+          	templateUrl: "/Home/Home",
+          	controller: 'homeCtrl'
+          })
+          .state('login', {
+          	url: "/login",
+          	templateUrl: "/Home/Login",
+          	controller: 'loginCtrl'
+          });
+    });
+
+App.controller('loginCtrl', function ($scope) {
+	$scope.login = '';
+	$scope.password = '';
+});
+
+App.controller('homeCtrl', function ($scope) {
+	$scope.worksList = [
+		{
+			Date: new Date(),
+			Title: '123',
+			Author: 'Автор1'
+		},
+		{
+			Date: new Date(),
+			Title: 'qwe',
+			Author: 'Автор2'
+		},
+		{
+			Date: new Date(),
+			Title: 'asd',
+			Author: 'Автор3'
+		},
+	];
+});
